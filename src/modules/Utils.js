c519fe85a482700a36e923d136d7a56299d054a7
Xtrendence <xtrendence@yahoo.com>
2021-11-23 21:12:42 +0000

    Colors can now be randomly picked and uniquely assigned to clients.

--- src/modules/Utils.js
+++ src/modules/Utils.js
@@ -7,7 +7,7 @@ module.exports = {
 
 		for(const networkInterface of Object.keys(interfaces)) {
 			for(const net of interfaces[networkInterface]) {
-				if(net.family === "IPv4" && !net.internal) {
+				if(net.family === "IPv4" && !net.internal && !networkInterface.toLowerCase().match("(vethernet|vmware|vm|area)")) {
 					if(!ips[networkInterface]) {
 						ips[networkInterface] = [];
 					}
@@ -21,12 +21,32 @@ module.exports = {
 		return ip;
 	},
 
-	getColor() {
-		const colors = require("./Colors");
-		return colors[this.randomBetween(0, 63)];
+	getColor(address, clients) {
+		let colors = require("./Colors");
+		let available = Object.keys(colors);
+
+		delete clients[address];
+
+		let ips = Object.keys(clients);
+		ips.map(ip => {
+			let index = available.indexOf([clients[ip]["color"]].toString());
+			if(index > -1) {
+				available.splice(index, 1);
+			}
+		});
+
+		let max = available.length - 1;
+
+		let random = this.randomBetween(0, max);
+
+		return { colors:colors[available[random]], index:available[random] };
 	},
 
 	randomBetween(min, max) {
 		return min + Math.floor(Math.random() * (max - min + 1));
+	},
+
+	removeKey(key, {[key]: _, ...rest}) {
+		return rest;
 	}
 }
